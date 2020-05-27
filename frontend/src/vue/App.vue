6ecbb878b6662054ca1831f2529f08e9f518e7b6
Georgiy Vlasov <wlasowegor@gmail.com>
2020-05-27 07:14:14 +0500

    Clean up remnants of the fullscreen button

--- frontend/src/vue/App.vue
+++ frontend/src/vue/App.vue
@@ -1,21 +1,17 @@
 <template>
-    <fullscreen ref="fullscreen">
         <div class="app">
             <AdsensePlaceholder>AdSense</AdsensePlaceholder>
             <CountersPractice/>
         </div>
-    </fullscreen>
 </template>
 
 <script>
     import CountersPractice from '../vue/CountersPractice.vue';
-    import fullscreen from 'vue-fullscreen';
     import Vue from 'vue';
     import {VueHammer} from 'vue2-hammer';
     import flexTouch from 'vue-flex-touch';
     import AdsensePlaceholder from "./AdsensePlaceholder.vue";
 
-    Vue.use(fullscreen);
     Vue.use(VueHammer);
     Vue.use(flexTouch);
     export default {
