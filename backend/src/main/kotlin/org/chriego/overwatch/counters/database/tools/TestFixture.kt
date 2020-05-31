e3cdcc670942b2d79ab2eef4bd6606a1eb3f619c
Georgiy Vlasov <wlasowegor@gmail.com>
2020-06-01 03:34:40 +0500

    Create a test fixture

---
+++ backend/src/main/kotlin/org/chriego/overwatch/counters/database/tools/TestFixture.kt
@@ -0,0 +1,40 @@
+package org.chriego.overwatch.counters.database.tools
+
+import org.chriego.overwatch.counters.Role
+import org.chriego.overwatch.counters.database.Hero
+import org.chriego.overwatch.counters.database.MatchupEvaluation
+import org.jetbrains.exposed.sql.Transaction
+
+class TestFixture : Fixture() {
+
+    override fun Transaction.load() {
+        val ana = Hero.new {
+            name = "Ana"
+            role = Role.Support
+        }
+        val reaper = Hero.new {
+            name = "Reaper"
+            role = Role.Damage
+        }
+        val reinhardt = Hero.new {
+            name = "Reinhardt"
+            role = Role.Tank
+        }
+        MatchupEvaluation.new {
+            `object` = ana
+            subject = reinhardt
+            score = -4
+        }
+        MatchupEvaluation.new {
+            `object` = ana
+            subject = reaper
+            score = 4
+        }
+        MatchupEvaluation.new {
+            `object` = reaper
+            subject = reinhardt
+            score = 8
+        }
+    }
+
+}
