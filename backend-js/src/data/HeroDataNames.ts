239a8bb0d2275410a9ac43d72ecb951699406e1a
Georgiy Vlasov <wlasowegor@gmail.com>
2020-06-29 09:45:01 +0500

    Extract hero data names to a separate enum

---
+++ backend-js/src/data/HeroDataNames.ts
@@ -0,0 +1,38 @@
+const enum HeroDataNames {
+    Ana = 'ana',
+    Ashe = 'ashe',
+    Baptiste = 'baptiste',
+    Bastion = 'bastion',
+    Brigitte = 'brigitte',
+    Dva = 'dva',
+    Doomfist = 'doomfist',
+    Echo = 'echo',
+    Genji = 'genji',
+    Hanzo = 'hanzo',
+    Junkrat = 'junkrat',
+    Lucio = 'lucio',
+    McCree = 'mccree',
+    Mercy = 'mercy',
+    Mei = 'mei',
+    Moira = 'moira',
+    Orisa = 'orisa',
+    Pharah = 'pharah',
+    Reaper = 'reaper',
+    Reinhardt = 'reinhardt',
+    Roadhog = 'roadhog',
+    Sigma = 'sigma',
+    Soldier = 'soldier',
+    Sombra = 'sombra',
+    Symmetra = 'symmetra',
+    Torbjorn = 'torbjorn',
+    Tracer = 'tracer',
+    Widowmaker = 'widowmaker',
+    Winston = 'winston',
+    WreckingBall = 'wreckingball',
+    Zarya = 'zarya',
+    Zenyatta = 'zenyatta'
+
+}
+
+export default HeroDataNames
+
