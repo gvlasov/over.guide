a75faa4c114d771f5c0b1c77a41a8054067827ed
Georgiy Vlasov <wlasowegor@gmail.com>
2020-10-08 00:30:43 +0500

    Disable going fullscreen when opening roster

--- frontend/src/vue/guides/tags/hero/TagBuilderRoster.vue
+++ frontend/src/vue/guides/tags/hero/TagBuilderRoster.vue
@@ -162,7 +162,7 @@ export default class TagBuilderRoster extends mixins(Roster_SelectedHeroesMixin)
 
     beforeMount() {
         if (typeof window.orientation !== 'undefined') {
-            document.documentElement.requestFullscreen()
+            // document.documentElement.requestFullscreen()
         }
     }
 }
