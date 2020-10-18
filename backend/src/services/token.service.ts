1d553bc1424e9d98d8d70633bf944084f776c605
Georgiy Vlasov <wlasowegor@gmail.com>
2020-10-18 08:25:22 +0500

    Fix first authentication request failing

--- backend/src/services/token.service.ts
+++ backend/src/services/token.service.ts
@@ -8,15 +8,15 @@ export class TokenService {
     CryptoJS = require('crypto-js')
 
     getToken(user: User): string {
-        return this.CryptoJS.AES.encrypt(
-            user.battleNetUserId,
+        return this.CryptoJS.Rabbit.encrypt(
+            user.battleNetUserId.toString(),
             TOKEN_CIPHER_SECRET
         ).toString();
     }
 
     async getUser(token: string): Promise<User | null> {
         // https://stackoverflow.com/a/22601457/1542343
-        const battleNetUserId = this.CryptoJS.AES.decrypt(
+        const battleNetUserId = this.CryptoJS.Rabbit.decrypt(
             token,
             TOKEN_CIPHER_SECRET
         ).toString(this.CryptoJS.enc.Utf8);
