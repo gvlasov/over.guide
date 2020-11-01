7defef1af6f42ed321301cf7668a64d00b9c5b01
Georgiy Vlasov <wlasowegor@gmail.com>
2020-11-02 01:36:26 +0500

    Implement spinner

---
+++ frontend/src/vue/Spinner.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="spinner"
+         v-bind:style="{backgroundPositionX: this.shiftPx+'px'}"
+    />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from "vue-class-component";
+
+@Component({})
+export default class Spinner extends Vue {
+
+    interval = null
+    shiftPx = 0;
+
+    mounted() {
+        this.interval = setInterval(
+            () => {
+                this.shiftPx += 40;
+                if (this.shiftPx > 21 * 40) {
+                    this.shiftPx = 0
+                }
+            },
+            1000/40
+        )
+    }
+
+    destroyed() {
+        clearInterval(this.interval)
+    }
+};
+
+</script>
+
+<style scoped>
+.spinner {
+    background-image: url('/icons/spinner-sprite-modified.png');
+    width: 40px;
+    height: 40px;
+    opacity: .5;
+}
+</style>
---
+++ frontend/src/vue/SpinnerBlock.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="spinner-block">
+        <Spinner/>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from "vue-class-component";
+import Spinner from "@/vue/Spinner.vue";
+
+@Component({
+    components: {
+        Spinner,
+    },
+})
+export default class SpinnerBlock extends Vue {
+};
+
+</script>
+
+<style scoped>
+.spinner-block {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+}
+</style>
