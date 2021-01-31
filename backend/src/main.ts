1c036e45e163e051d6bfe695c9588372d178d83f
Georgiy Vlasov <wlasowegor@gmail.com>
2021-01-31 18:55:00 +0500

    Disable https in nest backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,10 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {NestExpressApplication} from "@nestjs/platform-express";
-import {readFileSync} from 'fs'
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(
-        AppModule,
-        {
-            httpsOptions: {
-                key: readFileSync('./src/test/server.key'),
-                cert: readFileSync('./src/test/server.crt'),
-            }
-        }
+        AppModule
     );
     const cors = {
         origin: "*"
