507df34364d7b1276663ae1346c5a3dcaf5d8799
Georgiy Vlasov <wlasowegor@gmail.com>
2021-01-31 19:30:35 +0500

    Use backend.smol.guide instead of smol.guide:8080

--- frontend/build/config/prod.env.js
+++ frontend/build/config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   ENVIRONMENT: '"production"',
   BATTLE_NET_CLIENT_ID: '"9b012b6e1edf492085cfc5eded56adae"',
-  BATTLE_NET_REDIRECT_URI: '"https://smol.guide:8080/battlenet-auth"',
+  BATTLE_NET_REDIRECT_URI: '"https://backend.smol.guide/battlenet-auth"',
   IMGUR_CLIENT_ID:  '"546c25a59c58ad7"',
-  BACKEND_BASE_URL: '"https://smol.guide:8080"',
+  BACKEND_BASE_URL: '"https://backend.smol.guide"',
 };
