7e71fd8600bd8956bfb62a59cbd0295f79351586
Georgiy Vlasov <wlasowegor@gmail.com>
2021-01-31 17:31:32 +0500

    Make cypher secret more complex

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -3,5 +3,5 @@ export const BATTLE_NET_CLIENT_ID = '9b012b6e1edf492085cfc5eded56adae';
 export const BATTLE_NET_CLIENT_SECRET = 'Rif8Ug708T47DUw7rxftqCOuonwgETOu';
 export const FRONTEND_ROOT_URL = 'https://192.168.1.39:8000/'
 export const BATTLE_NET_REDIRECT_URI = "https://192.168.1.39:8080/battlenet-auth"
-export const TOKEN_CIPHER_SECRET = 'hello beautiful lady horse'
+export const TOKEN_CIPHER_SECRET = '9hello beau&tiful ladee horse!'
 export const SQL_LOG = false
