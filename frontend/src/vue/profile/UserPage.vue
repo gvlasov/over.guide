84792423045baa8fe57da6b1dc10dea231d06dcc
Georgiy Vlasov <wlasowegor@gmail.com>
2021-02-06 20:58:34 +0500

    Allow user to log out if he was deleted and his cookies no longer allow him to authenticate

--- frontend/src/vue/profile/UserPage.vue
+++ frontend/src/vue/profile/UserPage.vue
@@ -7,6 +7,19 @@
             Loading
         </div>
         <div
+                v-else-if="isDeletedUserOpeningHimself"
+                class="loading-notice"
+        >
+            <div>
+                <div class="deletion-notice">Your user was deleted</div>
+                <OverwatchButton
+                        type="default"
+                        v-hammer:tap="() => Auth.logoutSite()"
+                >Log out
+                </OverwatchButton>
+            </div>
+        </div>
+        <div
                 v-else-if="userInfo === null"
                 class="loading-notice"
         >
@@ -26,14 +39,19 @@ import Vue from 'vue'
 import Component from "vue-class-component";
 import AsyncComputedProp from 'vue-async-computed-decorator'
 import UserInfo from "./UserInfo.vue";
+import Authentication from "@/ts/Authentication";
+import OverwatchButton from "@/vue/OverwatchButton.vue";
 
 @Component({
     components: {
         UserInfo,
+        OverwatchButton,
     },
 })
 export default class UserPage extends Vue {
     declare $route: any
+
+    Auth = Authentication.instance
 
     declare userInfo: UserInfoVso | null
 
@@ -42,11 +60,15 @@ export default class UserPage extends Vue {
         return Backend.instance.getUserInfo(this.$route.params.id)
             .then(dto => new UserInfoVso(dto))
             .catch(e => {
-                throw e
                 if (e.response.status === 404) {
                     return new Promise((resolve, reject) => resolve(null))
                 }
             })
+    }
+
+    get isDeletedUserOpeningHimself() : boolean {
+        console.log(this.$route.params.id)
+        return this.userInfo === null && Authentication.instance.loggedIn && Authentication.instance.userId === Number.parseInt(this.$route.params.id)
     }
 }
 
@@ -60,5 +82,8 @@ export default class UserPage extends Vue {
     text-shadow: 0 0 .1em #333;
     color: white;
     font-family: BigNoodleTooOblique, sans-serif;
+    .deletion-notice {
+        font-size: .6em;
+    }
 }
 </style>
