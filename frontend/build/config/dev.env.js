8140083f6878f73fd2518931b98e1c525d52ac3d
Georgiy Vlasov <wlasowegor@gmail.com>
2021-02-21 19:41:18 +0500

    Move nodejs in dev env backend to 8082 host port to not conflict with sensu-backend

--- frontend/build/config/dev.env.js
+++ frontend/build/config/dev.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   ENVIRONMENT: '"dev"',
   BATTLE_NET_CLIENT_ID: '"9b012b6e1edf492085cfc5eded56adae"',
-  BATTLE_NET_REDIRECT_URI: '"https://192.168.1.39:8080/battlenet-auth"',
+  BATTLE_NET_REDIRECT_URI: '"https://192.168.1.39:8082/battlenet-auth"',
   IMGUR_CLIENT_ID:  '"546c25a59c58ad7"',
-  BACKEND_BASE_URL: '"https://192.168.1.39:8080"',
+  BACKEND_BASE_URL: '"https://192.168.1.39:8082"',
 };
